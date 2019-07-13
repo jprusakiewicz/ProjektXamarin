0b3726d832b980eb84f99ede7afb37b09ce5b90d
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2019-07-13 11:36:39 +0200

    New Filders, changed Project files structures - transformation into basic mvvm model

---
+++ ProjektXamarin/Views/InsuranceListPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjektXamarin.Views
+{
+    public partial class InsuranceListPage : ContentPage
+    {
+        public InsuranceListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
